a3bd7dd8c74e864c3389683023207f463f365b8a
William Woodruff <arcwilbo@crc-dot1x-nat-10-239-123-136.bu.edu>
2024-04-26 18:10:35 -0400

    tested stuff out

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-print(5)
+import yfinance as yf
+
+
+hist = yf.download("BTC-USD", period="max", interval="1d")
+print(hist.loc[:, hist.columns.str.contains('_\d+$')].iloc[-1].values.reshape(1, -1))
+#print(hist.index[-1])
